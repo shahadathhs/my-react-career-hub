d52256f5f396dae8543e943386abb87425b11423
Shahadath Hossen Sajib <shahadathhossensajib732@gmail.com>
2024-03-23 11:58:26 +0600

    show all job functional

--- src/components/FeaturedJobs/FeaturedJobs.jsx
+++ src/components/FeaturedJobs/FeaturedJobs.jsx
@@ -2,23 +2,34 @@ import { useEffect, useState } from "react";
 import Job from "../Job/Job";
 
 const FeaturedJobs = () => {
-
   const [jobs, setJobs] = useState([]);
+  const [dataLength, setDataLength] = useState(4);
 
   useEffect(() => {
-    fetch('jobs.json')
-      .then(res => res.json())
-      .then(data => setJobs(data));
-  }, [])
+    fetch("jobs.json")
+      .then((res) => res.json())
+      .then((data) => setJobs(data));
+  }, []);
 
   return (
-    <div  className="text-center p-5 space-y-4">
+    <div className="text-center p-5 space-y-4">
       <h3 className="text-5xl">Featured Jobs: {jobs.length}</h3>
-      <p className="text-gray-500">Explore thousands of job opportunities with all the information you need. Its your future</p>
+      <p className="text-gray-500">
+        Explore thousands of job opportunities with all the information you
+        need. Its your future
+      </p>
       <div className="text-left grid grid-cols-2 gap-5">
-        {
-          jobs.map(job => <Job key={job.id} job={job}></Job>)
-        }
+        {jobs.slice(0, dataLength).map((job) => (
+          <Job key={job.id} job={job}></Job>
+        ))}
+      </div>
+      <div className={dataLength === jobs.length ? "hidden" : ""}>
+        <button
+          onClick={() => setDataLength(jobs.length)}
+          className="btn btn-primary"
+        >
+          Show All Jobs
+        </button>
       </div>
     </div>
   );
